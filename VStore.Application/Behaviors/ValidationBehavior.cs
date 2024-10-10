2334e2a76e393775aaefcf3f3c9e28d3074e8f62
Phan Vu <anhvu2182004@gmail.com>
2024-10-10 09:47:06 +0700

    [ADD]: add validation behavior and handle result + error

---
+++ VStore.Application/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,31 @@
+using FluentValidation;
+using MediatR;
+using VStore.Domain.Shared;
+
+namespace VStore.Application.Behaviors;
+
+public class ValidationBehavior<TRequest, TResponse>(IValidator<TRequest>? validator = null)
+    : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<Result>
+{
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        if (validator is null)
+        {
+            return await next();
+        }
+
+        var validationResult = await validator.ValidateAsync(request, cancellationToken);
+
+        if (validationResult.IsValid)
+        {
+            return await next();
+        }
+
+        var errors = validationResult.Errors
+            .Select(error => new Error(error.ErrorCode, error.ErrorMessage))
+            .ToArray();
+
+        return (dynamic)new ValidationError(errors);
+    }
+}
