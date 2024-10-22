9e3dd89b41bae6e549897cf99a907cb8294491e4
Phan Vu <anhvu2182004@gmail.com>
2024-10-22 16:46:51 +0700

    [UPDATE]: Modify brand

--- VStore.Domain/Abstractions/Repositories/IBrandRepository.cs
+++ VStore.Domain/Abstractions/Repositories/IBrandRepository.cs
@@ -4,4 +4,5 @@ namespace VStore.Domain.Abstractions.Repositories;
 
 public interface IBrandRepository : IRepositoryBase<Brand, int>
 {
+    Task<bool> IsBrandHasProductAsync(int brandId, CancellationToken cancellationToken = default);
 }
