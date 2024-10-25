4efc24831871dcaa47b616240fbad25cd492dd93
Phan Vu <anhvu2182004@gmail.com>
2024-10-25 12:17:21 +0700

    [ADD]: Add migration

---
+++ VStore.Persistence/Migrations/20241025041641_Add CustomerAddress.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VStore.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCustomerAddress : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "WardCode",
+                table: "CustomerAddresses",
+                type: "nvarchar(255)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "WardCode",
+                table: "CustomerAddresses",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)");
+        }
+    }
+}
