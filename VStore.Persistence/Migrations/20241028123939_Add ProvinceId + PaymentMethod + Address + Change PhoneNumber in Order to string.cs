ed47d469166f4d537cac83bb83c9b7744b97bc70
Phan Vu <anhvu2182004@gmail.com>
2024-10-29 12:33:55 +0700

    [ADD]: Add migrations

---
+++ VStore.Persistence/Migrations/20241028123939_Add ProvinceId + PaymentMethod + Address + Change PhoneNumber in Order to string.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VStore.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProvinceIdPaymentMethodAddressChangePhoneNumberinOrdertostring : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Orders",
+                type: "nvarchar(255)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Orders");
+        }
+    }
+}
