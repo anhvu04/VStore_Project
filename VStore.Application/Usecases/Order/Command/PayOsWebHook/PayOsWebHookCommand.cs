3ccbb535192aa5488ba6250d70ae3a8d5de5186b
Phan Vu <anhvu2182004@gmail.com>
2024-10-30 14:52:54 +0700

    [ADD]: Add webhook for payos

--- VStore.Application/Usecases/Order/Command/PayOsWebHook/PayOsWebHookCommand.cs
+++ VStore.Application/Usecases/Order/Command/PayOsWebHook/PayOsWebHookCommand.cs
@@ -4,7 +4,7 @@ namespace VStore.Application.Usecases.Order.Command.PayOsWebHook;
 
 public record PayOsWebHookCommand : ICommand
 {
-    public required string Codee { get; set; }
+    public required string Code { get; set; }
     public required string Desc { get; set; }
     public required bool Success { get; set; }
     public required object Data { get; set; }
